8e1d0522c6ac16901a938c3c8ee0ec74e8220fc0
William Fernandes <wiilfern1910@outlook.com>
2025-06-24 08:45:16 -0300

    Initial commit

---
+++ src/test/java/com/liamfer/markdownAPI/MarkdownApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liamfer.markdownAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
