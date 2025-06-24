33e2e98f77d6c46c44634c2e15e40bc35e031849
William Fernandes <wiilfern1910@outlook.com>
2025-06-24 09:03:33 -0300

    feat: Notes Endpoints

--- src/main/java/com/liamfer/markdownAPI/controller/NoteController.java
+++ src/main/java/com/liamfer/markdownAPI/controller/NoteController.java
@@ -1,9 +1,32 @@
 package com.liamfer.markdownAPI.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.liamfer.markdownAPI.domain.NoteEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notes")
 public class NoteController {
+
+    @PostMapping()
+    public ResponseEntity<?> addNote(){
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping("/grammar-check")
+    public ResponseEntity<?> checkGrammar(){
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getNotes(){
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/{id}/rendered")
+    public ResponseEntity<?> getRenderedNote(@PathVariable("id") Long id){
+        return ResponseEntity.ok("");
+    }
+
+
 }
